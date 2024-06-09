4e2e7c9e536fa3f6d1cf7697f5e94e6e2016f097
Drew Herron <herron@mailbox.org>
2024-06-09 12:10:16 -0700

    Using saved class_indices

--- fz_predict_font.py
+++ fz_predict_font.py
@@ -6,15 +6,25 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
 
-def predict_font(image_path, model_path='font_recognition_model.keras'):
+def load_class_indices(path='class_indices.json'):
+    import json
+    with open(path, 'r') as json_file:
+        class_indices = json.load(json_file)
+    return class_indices
+
+def predict_font(image_path, model_path='font_recognition_model.keras', indices_path='class_indices.json'):
     model = load_model(model_path)
+    class_indices = load_class_indices(indices_path)
 
     img = load_img(image_path, target_size=(64, 64))
     img_array = img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
     img_array /= 255.0
 
-    # labeled_predictions = yadda yadda yadda
+    prediction = model.predict(img_array)[0]
+    class_labels = {v: k for k, v in class_indices.items()}
+    labeled_predictions = [(class_labels[i], float(prob)) for i, prob in enumerate(prediction)]
+    labeled_predictions.sort(key=lambda x: x[1], reverse=True)
 
-    return labeled_predictions
-
+    # Return the top 5 predictions
+    return labeled_predictions[:5]
